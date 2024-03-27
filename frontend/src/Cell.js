5ef4423576454a862af04cf3c1f69340ae4a5423
kayan9896 <90473306+kayan9896@users.noreply.github.com>
2024-03-27 17:57:17 +0000

    style

--- frontend/src/Cell.js
+++ frontend/src/Cell.js
@@ -3,7 +3,8 @@ import React from 'react';
 import './App.css'; // Import your stylesheet
 
 function Cell({ value, revealed, flagged, onLeftClick, onRightClick}) {
-  let displayValue = revealed ? value : ''; // Show value if revealed
+  if (value==='M') value='*'
+  let displayValue = (revealed&&value!=='0') ? value : ''; // Show value if revealed
   if (flagged&&!revealed) {
     displayValue = '🚩'; // Display a flag
   }
